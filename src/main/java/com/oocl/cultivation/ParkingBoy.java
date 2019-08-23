f3e517c30e887076308a62c605653bcba5e6c503
yangjingsheng <673630886@qq.com>
2019-08-23 18:12:46 +0800

    第一个测试

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -11,12 +11,14 @@ public class ParkingBoy {
 
     public ParkingTicket park(Car car) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+//        throw new RuntimeException("Not implemented");
+    	return parkingLot.park(car);
     }
 
     public Car fetch(ParkingTicket ticket) {
         // TODO: Please implement the method
-        throw new RuntimeException("Not implemented");
+//        throw new RuntimeException("Not implemented");
+    	return parkingLot.fetch(ticket);
     }
 
     public String getLastErrorMessage() {
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,18 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private final int capacity;
-    private Map<ParkingTicket, Car> cars = new HashMap<>();
+	private final int capacity;
+	private Map<ParkingTicket, Car> cars = new HashMap<>();
 
-    public ParkingLot() {
-        this(10);
-    }
+	public ParkingLot() {
+		this(10);
+	}
 
-    public ParkingLot(int capacity) {
-        this.capacity = capacity;
-    }
+	public ParkingLot(int capacity) {
+		this.capacity = capacity;
+	}
 
-    public int getAvailableParkingPosition() {
-        return cars.size() - capacity;
-    }
+	public int getAvailableParkingPosition() {
+		return capacity - cars.size();
+	}
+
+	public ParkingTicket park(Car car) {
+		ParkingTicket ticket = new ParkingTicket();
+		cars.put(ticket, car);
+		return ticket;
+	}
+
+	public Car fetch(ParkingTicket ticket) {
+		Car car = new Car();
+		car = cars.get(ticket);
+		return car;
+	}
+
 }
